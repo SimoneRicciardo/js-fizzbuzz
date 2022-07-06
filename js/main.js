4577ad3f900521b91d06eb36813d9cebe5caa954
Ricciardo Simone <ricciardo855@gmail@com>
2022-07-06 18:15:03 +0200

    fatto con multipli di 5+3 manca per quelli  5 e 3

--- js/main.js
+++ js/main.js
@@ -3,10 +3,21 @@ const containerDom = document.getElementById('container')
 for (let i = 1; i <= 100; i++) {
     containerDom.innerHTML += `<div class="box"> ${i} </div>`;
 
-   if (i % 3 == 0){
-    
+    let box = document.querySelector('.box:last-child')
+
+   if (i % 3 ==0){
+     box.classList.add('green');
+     box.innerHTML = 'fizz';
    }
 
-    
+   else if (i % 5 ==0){
+    box.classList.add('yellow');
+    box.innerHTML = 'buzz';
+  }
+
+  else if (i %  ==0) {
+    box.classList.add('red');
+    box.innerHTML = 'fizz';
+  }
 
 }
