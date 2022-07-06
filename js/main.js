950ddb61f5cc6b947c135991cc7bb9fd7883a2e6
Ricciardo Simone <ricciardo855@gmail@com>
2022-07-06 16:41:32 +0200

    style + base js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const containerDom = document.getElementById('container')
+
+for (let i = 1; i <= 100; i++) {
+    containerDom.innerHTML += `<div class="box"> ${i} </div>`;
+
+   if (i % 3 == 0){
+    
+   }
+
+    
+
+}
